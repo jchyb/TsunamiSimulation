dcc6d6e426050f743ac728fa4b00d0db5d4553fa
MrDenkoV <mr.denkov@gmail.com>
2020-05-24 00:04:31 +0200

    Typo

--- src/main/scala/SimElements/World.scala
+++ src/main/scala/SimElements/World.scala
@@ -2,7 +2,7 @@ package SimElements
 
 import Utils.Vector2
 
-class World(private val simTimeInSeconds : Int, rece){
+class World(private val simTimeInSeconds : Int){
   var water : Water = _
 
   def set(wavePosition : Vector2[Int], waveStrength : Double): Unit = {
