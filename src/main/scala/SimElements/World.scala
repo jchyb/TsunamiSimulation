1a7a23b7a54fa7b3816c1b811acec90ba722b94e
MrDenkoV <mr.denkov@gmail.com>
2020-05-27 10:47:39 +0200

    Minor fix

--- src/main/scala/SimElements/World.scala
+++ src/main/scala/SimElements/World.scala
@@ -17,16 +17,9 @@ class World(private val simTime : Int, receiver : Receiver){
   // only before setWave
   def addBreakwater(position: Vector2[Int], r: Double, height: Double): Unit = {
     val breakwater = Breakwater(position, r, height)
-//    var list = List()
-//    for(i <- (-r).toInt to r.toInt; j <- (-r).toInt to r.toInt){
-//      if()
-//    }
     var list: List[Vector2[Int]] = for(i <- ((-r).toInt to r.toInt).toList; j <- ((-r).toInt to r.toInt).toList) yield Vector2[Int](i, j)
-//    var list: List[Vector2[Int]] = ((-r).toInt to r.toInt).toList.flatMap(x => ((-r).toInt to r.toInt).toList.flatMap(y => (x, y)))
-//    list.filter((position+_).length()<=r)
-//    list.filter((e:Vector2[Int]) => (position+e).length()<=r)
-    list = list.filter(e => (position+e).length()<=r)
-    breakwatersMap.addAll(list.zip(List.fill(list.length)(breakwater)))
+    list = list.filter(_.length()<=r)
+    breakwatersMap.addAll(list.map(_+position).zip(List.fill(list.length)(breakwater)))
   }
 
   def setWave(wavePosition : Vector2[Int], waveStrength : Double): Unit = {
